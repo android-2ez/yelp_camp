cad95513c33dda3e79b0f642ed0241cfbe8f0111
Mint <legolas.vnz@gmail.com>
2016-07-11 02:46:10 +0700

    Neu vi / thi t quy

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+var port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started! " + port);
 });
